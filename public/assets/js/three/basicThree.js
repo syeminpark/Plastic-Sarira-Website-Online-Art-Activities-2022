dd0588c7556369b56f47122483e69a41eb42e9e0
syeminPark <soccer0874@gmail.com>
2022-11-13 16:03:57 +0900

    make version

--- public/assets/js/three/basicThree.js
+++ public/assets/js/three/basicThree.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
 import {
     OrbitControls
 } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js"
