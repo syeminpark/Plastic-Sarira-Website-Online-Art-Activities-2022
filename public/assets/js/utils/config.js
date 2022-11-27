b76525455765c3616436dab398b174ba966294a0
syeminPark <soccer0874@gmail.com>
2022-11-28 01:20:20 +0900

    파라미터 미세 조정

--- public/assets/js/utils/config.js
+++ public/assets/js/utils/config.js
@@ -10,13 +10,58 @@ export default{
     initialMaxPlasticCount:10,
     plasticScale:0.3,
     //각 폐플라스틱 사이 간격 
-    plasticOffsetRange:0.13,
+    plasticOffsetRange:0.15,
     //흐름(속력+방향)
-    velMin: 0.003,
+
+    velMin: 0.001,
 
     ///////////////////////////////////////////////////
     //life 
-    lifespan:300,
+
+    lifespan:700,
+
+    //생물체 크기?
+
+    //라이프바 초기 y 위치?? 
+
+    //controls
+    lerpSpeed : 0.03,
+
+    ///////////////////////////////////////////////////
+    //materials
+
+    particleSize: 0.3,
+
+     ///////////////////////////////////////////////////
+    //server
+    type:"administrator",
+    sariraArchiveMax:30
+}
+
+
+//베타 때 파라미터 수치 
+/*
+export default{
+
+    ///////////////////////////////////////////////////
+    //world 
+
+    worldSize:300,
+    //최대 파티클 개수 
+    maxParticleCount:15000,
+    //처음에 불러오는 폐플라스틱 개수 
+    initialMaxPlasticCount:10,
+    plasticScale:0.3,
+    //각 폐플라스틱 사이 간격 
+    plasticOffsetRange:0.15,
+    //흐름(속력+방향)
+
+    velMin: 0.001,
+
+    ///////////////////////////////////////////////////
+    //life 
+
+    lifespan:700,
 
     //생물체 크기?
 
@@ -37,3 +82,4 @@ export default{
     sariraArchiveMax:30
 
 }
+*/
